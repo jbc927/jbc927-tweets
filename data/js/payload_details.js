4bf7f70ad15db55ad63f621c5432ef85a745e169
jbc927 <jbc@justincooley.com>
2016-12-30 10:43:11 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
-var payload_details =  {
-  "tweets" : 14053,
-  "created_at" : "2016-12-30 17:19:22 +0000",
-  "lang" : "en"
+var payload_details = {
+	"tweets": 14054,
+	"created_at": "Fri Dec 30 18:43:10 +0000 2016",
+	"lang": "en"
 }
